5e639d73a5445e845710de47df31a86b8c772899
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-10-27 17:50:29 +0400

    The Module Pattern

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 // console.log(price, tq);
 
 console.log('Importing module');
+
 // console.log(shippingCost);
 
 // import * as ShoppingCart from './shoppingCart.js';
@@ -31,6 +32,7 @@ console.log('Importing module');
 // console.log(data);
 // console.log('Something');
 
+/*
 const getLastPost = async function () {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
   const data = await res.json();
@@ -46,3 +48,34 @@ console.log(lasPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+
+*/
+// The Module Pattern
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
